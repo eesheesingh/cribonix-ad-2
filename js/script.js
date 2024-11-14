24f6d788b7c4a8138d9cd8029dd0568a799c701f
eesheesingh <chiro.singh9769@gmail.com>
2024-11-14 13:39:24 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const observer = new IntersectionObserver((entries) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add('active');
+//             }
+//         });
+//     }, { threshold: 0.8 });
+
+//     document.querySelectorAll('.scroll-animation').forEach(section => {
+//         observer.observe(section);
+//     });
+// });
